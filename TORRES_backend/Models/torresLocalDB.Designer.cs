7b0390dbde670f873ffc5f197628c5cd11ea6709
Kevin John Insigne <kjlinsigne@amaes.edu.ph>
2021-09-23 01:10:47 +0800

    Add API Fetch All Training

--- TORRES_backend/Models/torresLocalDB.Designer.cs
+++ TORRES_backend/Models/torresLocalDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\devop\source\repos\torres_technology_corp_backend\repo_torres_backend\TORRES_backend\Models\torresLocalDB.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Kejo\Torres_Tech\torrestech_backend\repo_torres_backend\TORRES_backend\Models\torresLocalDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
