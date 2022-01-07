70137344e0f1cd9c0d3c38ae00b6b13f046d7d3b
jm sevilla <miggysvll@gmail.com>
2022-01-08 00:50:54 +0800

    connected backend ttc-57

--- TORRES_backend/Controllers/v3/NavbarDynamicController.cs
+++ TORRES_backend/Controllers/v3/NavbarDynamicController.cs
@@ -6,12 +6,15 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using TORRES_backend.Helpers.v3;
+using TORRES_backend.Models;
+using TORRES_backend.APIConnection;
 namespace TORRES_backend.Controllers.v3
 {
-    [RoutePrefix("api/navbar-dynamic")]
+    [RoutePrefix("api/v3/navbar-dynamic")]
     public class NavbarDynamicController : ApiController
     {
         NavbarHelper navFunction = new NavbarHelper();
+        private dbtorresEntities core;
         [Route("get-content"), HttpGet]
         public async Task<IHttpActionResult> getNavbarContent()
         {
@@ -21,6 +24,24 @@ namespace TORRES_backend.Controllers.v3
                 navFunction.getNavbarUI();
                 result = Ok(NavbarHelper.response);
                 return await Task.FromResult(result);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [Route("get-content/{navbarIdentifier}"), HttpGet]
+        public IHttpActionResult getNavbarIdentifier(int navbarIdentifier)
+        {
+            try
+            {
+                using(core = Connection._publiccloud)
+                {
+                    var obj = core.navbar_identifier.Where(x => x.navbarIdentifier
+                    == navbarIdentifier).ToList();
+                    return Ok(obj);
+                }
             }
             catch (Exception)
             {
